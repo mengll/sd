56c143ac002da8dfb3e42775f336f2b8ddbc6296
mengll <1633094010@qq.com>
2015-08-18 17:37:20 +0800

    Create cocos2d.js

---
+++ cocos2d.js
@@ -0,0 +1,16 @@
+//cocos2d-js 的学习笔记
+创建精灵
+var sprite = new cc.sprite('');
+this.addChild(sprite,Tag);
+//设置相关的属性
+sprite.attr({x:'',y:2,'scaleY':2});
+//设置当前的对象是否可见
+sprite.visible = false;//
+//获取的当前的窗口的大小
+var windowSize = director.getWinSize(); //
+//用多长的事件运行到怎样的结果
+  var actionTo = cc.moveTo(2, cc.p(s.width - 40, s.height - 40));
+//run an action 
+this._tamara.runAction(actionTo);
+//串行化的执行
+    this._grossini.runAction(cc.sequence(actionBy, actionByBack));
